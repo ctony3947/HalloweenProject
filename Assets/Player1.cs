627809575d603269efc622a15bf6f787cf280c41
ctony3947 <120767822+ctony3947@users.noreply.github.com>
2023-10-21 15:02:16 -0400

    SomeAddedStuff

---
+++ Assets/Player1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    private int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void addScore(int sc)
+    {
+        score += sc;
+    }
+}
